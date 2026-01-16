a9b8504eb954b387771f0c70bcf50ac8ac5ec4ab
Soham <sohamsutar9960@gmail.com>
2026-01-16 18:01:01 +0530

    Add Swagger configuration for API documentation

---
+++ TcUserApp-Backend/src/main/java/intelizign/tcuserapp/backend/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package intelizign.tcuserapp.backend.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("TCUserApp API")
+                        .description("Teamcenter User Management Application")
+                        .version("1.0"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("Authorization")));
+    }
+}
